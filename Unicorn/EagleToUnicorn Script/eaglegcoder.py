4761145416c11ef33c1a2e9b668b210bdadca09f
Will Langford <langfordw@gmail.com>
2010-08-20 22:37:13 -0500

    uploading eagle to unicorn script

---
+++ Unicorn/EagleToUnicorn Script/eaglegcoder.py
@@ -0,0 +1,45 @@
+
+from optparse import OptionParser
+from Gerber import Gerber
+
+def main():
+    usage = "usage: %prog [options] filename > output.gcode"
+    
+    parser = OptionParser(usage)
+    parser.add_option("--feedrate", dest="feed_rate", default=2000,
+                      help="x and y feedrate")
+    parser.add_option("--servoup", dest="servo_up", default=50,
+                      help="servo's up position")
+    parser.add_option("--servodown", dest="servo_down", default=40,
+                      help="servo's down position")
+    parser.add_option("--linewidth", dest = "line_width", default=0.3,
+                      help="line width of pen")
+    parser.add_option("--startdelay", dest = "start_delay", default=50,
+                      help="delay after putting pen down")
+    parser.add_option("--stopdelay", dest = "stop_delay", default=150,
+                      help="delay after bringing pen up")
+    
+    (options, args) = parser.parse_args()
+    
+    if len(args) < 1:
+        parser.error("incorrect number of arguments")
+        
+    try:
+        gerber_file = open( args[0] )
+    except:
+        print "Could not open file" 
+        return 
+    
+    
+    gerber = Gerber(gerber_file, float(options.feed_rate), 
+                    float(options.servo_up), float(options.servo_down), 
+                    float(options.line_width), float(options.start_delay), 
+                    float(options.stop_delay))
+    gerber.parse_file(None)
+    gerber.output_GCODE()
+    
+    if(gerber_file != None):
+        gerber_file.close()
+                            
+if __name__=='__main__':
+    main()
