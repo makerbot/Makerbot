a57714056ac11224b4efe9471067b3450415e6b0
phooky <phooky@gmail.com>
2011-10-07 13:58:43 -0400

    added diamond pattern drilling

--- scripts/grid_driller/grid_driller.py
+++ scripts/grid_driller/grid_driller.py
@@ -11,12 +11,14 @@ def makeParser():
                         help='The depth of the holes to drill in mm')
     parser.add_argument('-v', type=float, default=5.0,
                         help='The hover height in mm')
-    parser.add_argument('--ft', type=float, default=500.0,
+    parser.add_argument('--ft', type=float, default=1000.0,
                         help='The travel feedrate')
-    parser.add_argument('--fd', type=float, default=5.0,
+    parser.add_argument('--fd', type=float, default=80.0,
                         help='The drill feedrate')
     parser.add_argument('-n', type=float, default=5,
                         help='The distance between hole centers')
+    parser.add_argument('-D', action='store_true', default=False,
+                        help='"Diamond" pattern (holes in face centers)')
     return parser
 
 
@@ -33,10 +35,10 @@ def gcMoveTo(x,y):
     global feed_travel
     return "G0 X{0} Y{1} F{2} (Go to {0},{1})\n".format(x,y,feed_travel)
 
-def gcHoleGrid():
+def gcHoleGrid(xinit=0.0,yinit=0.0):
     global width, height, density
-    x = 0.0
-    y = 0.0
+    x = xinit
+    y = yinit
     r = "(Drill grid start)\n"
     while x <= width:
         while y <= height:
@@ -44,8 +46,15 @@ def gcHoleGrid():
             r = r + gcDrillHole()
             y = y + density
         x = x + density
-        y = 0.0
+        y = yinit
     return r
+
+prefix = """
+G21 (Set to metric mode)
+"""
+
+suffix = """
+"""
 
 parser = makeParser()
 arguments = parser.parse_args()
@@ -56,4 +65,8 @@ hover = arguments.v
 feed_travel = arguments.ft
 feed_drill = arguments.fd
 density = arguments.n
+print prefix
 print gcHoleGrid()
+if (arguments.D):
+    print gcHoleGrid(density/2.0,density/2.0)
+print suffix
